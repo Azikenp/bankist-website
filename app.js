2cf26cc0298fceef1f109c662c06e0a41d9a4bbe
Azikenp <azikenp26@gmail.com>
2023-06-28 18:41:28 +0100

    tabbed components display

--- app.js
+++ app.js
@@ -72,6 +72,28 @@ document.querySelector('.nav__links').addEventListener('click', function(e){
 
 
 
+//Tabbed components
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function(e){
+  const clicked = e.target.closest('.operations__tab');
+  //Guard clause
+  if(!clicked) return;
+
+  //Active tab
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  //Activate content area
+  tabContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+});
+
+
+
 
 
 
@@ -84,6 +106,11 @@ document.querySelector('.nav__links').addEventListener('click', function(e){
 //////////////////////////////////////////////////
 
 //Practise
+
+const h1 = document.querySelector('h1');
+
+
+console.log(h1.childNodes);
 
 
 
